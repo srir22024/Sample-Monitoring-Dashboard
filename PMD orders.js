31f9ca081e27e1fd43744ba5659552de82f99ee0
SRIRAMAN <119477206+srir22024@users.noreply.github.com>
2024-07-05 13:56:30 +0530

    Add files via upload

---
+++ PMD orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Retro Graphic Hoodie ',
+        productNumber: '85743',
+        paymentStatus: '₹54,000',
+        status: 'None'
+    },
+    {
+        productName: 'Tailored Trench Coat',
+        productNumber: '97245',
+        paymentStatus: '-₹24,000',
+        status: 'Loss'
+    },
+    {
+        productName: 'StyPantslish Palazzo',
+        productNumber: '36452',
+        paymentStatus: '₹32,000',
+        status: 'profit'
+    },
+]
